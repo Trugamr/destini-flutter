b1b7577dccbe7a0f23e5ea8ca9ff6e5925e0e2fd
Trugamr <6trugamr9@gmail.com>
2020-06-17 19:02:34 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DestiniApp());
+}
+
+class DestiniApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: GamePage(),
+        ),
+      ),
+    );
+  }
+}
+
+class GamePage extends StatefulWidget {
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Destini'),
+    );
+  }
+}
