ddf1634759b98a236b183a161b17df6082c005d9
Trugamr <6trugamr9@gmail.com>
2020-06-17 19:55:03 +0530

    updated layout design

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,17 @@ class DestiniApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.yellow.shade50,
         body: SafeArea(
-          child: GamePage(),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/bg.jpg'),
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            child: GamePage(),
+          ),
         ),
       ),
     );
@@ -23,10 +32,77 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  RaisedButton buildKey(
+      {String text, Color color, VoidCallback onPressed, Color textColor}) {
+    return RaisedButton(
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 16.0,
+          color: textColor,
+        ),
+      ),
+      onPressed: onPressed,
+      color: color,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Destini'),
+    return Column(
+      children: <Widget>[
+        Expanded(
+          flex: 12,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'This is where the main story text will go depending on which the user will be able to follow a path.',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+            padding: EdgeInsets.only(
+              top: 16.0,
+              bottom: 6.0,
+              left: 16.0,
+              right: 16.0,
+            ),
+            child: buildKey(
+              text: 'First choice to follow',
+              color: Colors.yellow,
+              onPressed: () => print('first option'),
+              textColor: Colors.black,
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+            padding: EdgeInsets.only(
+              top: 6.0,
+              bottom: 16.0,
+              left: 16.0,
+              right: 16.0,
+            ),
+            child: buildKey(
+                text: 'Second choice to follow',
+                color: Colors.pink.shade400,
+                onPressed: () => print('second option'),
+                textColor: Colors.white),
+          ),
+        ),
+      ],
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
     );
   }
 }
